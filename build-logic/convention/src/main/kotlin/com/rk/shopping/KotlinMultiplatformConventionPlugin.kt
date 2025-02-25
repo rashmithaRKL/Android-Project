10ce464daa4fce2cbaa8e87e27f519edc951383a
CodeSandbox <support@codesandbox.io>
2025-02-25 06:06:33 +0000

    Fix KotlinMultiplatformConventionPlugin package and imports

--- build-logic/convention/src/main/kotlin/com/rk/shopping/KotlinMultiplatformConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/rk/shopping/KotlinMultiplatformConventionPlugin.kt
@@ -1,3 +1,10 @@
+package com.rk.shopping
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
 class KotlinMultiplatformConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         with(pluginManager) {
